00f12fd4ef42baf6468cc5484613ef7746a94034
SherlockNian <githubnian16@gmail.com>
2022-07-28 16:18:16 +0800

    Initial Commit

---
+++ LifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by Hong-Nian Lin 
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        print("loading")
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
